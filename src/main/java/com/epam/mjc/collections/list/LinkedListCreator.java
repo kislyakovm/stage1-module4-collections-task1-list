f14856e4fe5b4f9178023d7a66110af5711b89e4
kislyakovm <kislyakovm97@gmail.com>
2024-01-15 21:15:16 +0600

    update

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            if (sourceList.get(i) % 2 != 0) {
+                list.add(0, sourceList.get(i));
+            } else {
+                list.add(sourceList.get(i));
+            }
+        }
+        return list;
     }
 }
